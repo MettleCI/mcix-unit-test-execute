5ca4d5201d16992645829afe2038db9fc497609c
Stuart Norton <stuart.norton@datamigrators.com>
2026-03-31 17:31:16 +1100

    Sort exception check for all actions and remove extra debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -148,10 +148,6 @@ write_step_summary() {
       gh_warn "JUnit summarizer for '${MCIX_CMD_NAME}' failed" "Continuing without failing the action."
   fi
 
-  find / -name "cli.*.log" -ls
-
-  echo "looking for ${MCIX_LOG_DIR}/cli.$(date +%F).log"
-
   if [[ -f "${MCIX_LOG_DIR}/cli.$(date +%F).log" ]]; then
     {
       # Display the contents of the mcix command's log file. (collapsed by default)
@@ -167,7 +163,7 @@ write_step_summary() {
       gh_warn "Log file for '${MCIX_CMD_NAME}' not found" "Continuing without failing the action."
   fi
 
-  for file in "$MCIX_LOG_DIR/exception.*.log"; do
+  for file in $MCIX_LOG_DIR/exception.*.log; do
     if [ -f "$file" ]; then
       {
         # Display the contents of the mcix command's log file. (collapsed by default)
@@ -180,7 +176,7 @@ write_step_summary() {
         echo '</details>'
       } >>"$GITHUB_STEP_SUMMARY"
     fi
-  done
+  done 2>/dev/null
 }
 
 
